cc7905d767a7cf3533e749e77461c83f9540d2fc
Abdullahil Baki Adol <41238152+BakiAdol@users.noreply.github.com>
2019-10-23 07:54:29 +0600

    Update 02_coinChange.cpp

--- DP/02_coinChange.cpp
+++ DP/02_coinChange.cpp
@@ -1,3 +1,25 @@
+/*
+* Minimum coin need for make chane a certain ammount, unsing Bottom-Up Dynamic Techonic
+* Complexity O(amount*coins)
+*/
+int minimumCoin(int amount, int coins[], int numOfCoins)
+{
+    int requariedCoin[amount+1];
+    requariedCoin[0]=0;
+
+    for(int am=1;am<=amount;am++)
+    {
+        requariedCoin[am]=INT_MAX;
+        for(int coin=0;coin<numOfCoins;coin++)
+        {
+            requariedCoin[am]=min(requariedCoin[am],requariedCoin[am-coins[coin]]+1);
+        }
+    }
+    return requariedCoin[amount];
+}
+//--------------------------------------------------------------------------------------------
+
+
 // total waya of make amount using coins not followed order
 // Complexity O(2^n)
 int coinCombo(int amount, int coins[], int lenOfCoins)
