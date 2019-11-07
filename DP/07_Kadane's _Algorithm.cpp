94e72a6d32b1b9b7dde3640e359d48493f7ba1e5
Abdullahil Baki Adol <41238152+BakiAdol@users.noreply.github.com>
2019-11-07 19:54:21 +0600

    Update 07_Kadane's _Algorithm.cpp

--- DP/07_Kadane's _Algorithm.cpp
+++ DP/07_Kadane's _Algorithm.cpp
@@ -35,3 +35,92 @@ void CSS()
     cout << "Maximum Sum = " << maxSum << endl;
 }
 
+//----------------------------------------------------------------------------------------------//
+
+// Maximum sum rectangle in a 2D matrix
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define row 4
+#define col 5
+
+void kadane(int rowSome[], int* maxSum, int* top, int* bottom)
+{
+
+    int currentSum=rowSome[0];
+    *maxSum=rowSome[0];
+    *top=0;
+    *bottom=0;
+
+    for(int i=1;i<row;i++)
+    {
+        if(currentSum+rowSome[i]>rowSome[i])
+        {
+            currentSum+=rowSome[i];
+            if(*maxSum<currentSum)
+            {
+                *maxSum=currentSum;
+                *bottom=i;
+            }
+        }
+        else
+        {
+            currentSum=rowSome[i];
+            if(*maxSum<currentSum)
+            {
+                *maxSum=currentSum;
+                *top=*bottom=i;
+            }
+        }
+    }
+
+}
+
+void maxRectangleSum(int mat[row][col])
+{
+    int maxRectSum=INT_MIN,currentMaxSum;
+    int fLeft, fRight, fTop, fBottom, currentTop, currentBottom;
+    int rowSum[row];
+
+    for(int left=0;left<col;left++)
+    {
+        memset(rowSum,0,sizeof(rowSum));
+        for(int right=left;right<col;right++)
+        {
+            for(int i=0;i<row;i++)
+            {
+                rowSum[i]+=mat[i][right];
+            }
+
+            kadane(rowSum, &currentMaxSum, &currentTop, &currentBottom);
+
+            if(maxRectSum<currentMaxSum)
+            {
+                maxRectSum=currentMaxSum;
+                fTop=currentTop;
+                fBottom=currentBottom;
+                fLeft=left;
+                fRight=right;
+            }
+        }
+    }
+    cout << "Top Left Corner = (" << fTop << ", " << fLeft << ")" << endl;
+    cout << "Bottom Right Corner = (" << fBottom << ", " << fRight << ")" << endl;
+    cout << "Maximum Rectangle Sum = " << maxRectSum << endl;
+}
+
+int main()
+{
+    int mat[row][col] = {{ 1,  2, -1, -4, -20},
+                       {  -8, -3,  4,  2,   1},
+                       {   3,  8, 10,  1,   3},
+                       {  -4, -1,  1,  7,  -6}};
+                       
+    maxRectangleSum(mat);
+
+    return 0;
+}
+
+
