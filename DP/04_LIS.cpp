8e6944c6e42e4452a17385f1575c5b0a114b3980
Abdullahil Baki Adol <41238152+BakiAdol@users.noreply.github.com>
2020-03-29 16:37:46 +0600

    Update 04_LIS.cpp

--- DP/04_LIS.cpp
+++ DP/04_LIS.cpp
@@ -1,21 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
 
-int LIS(int arr[], int n)
+int ar[]={2,8,3,5,9,6,7,10};
+int n=8;
+
+// dp iterative method
+void LISiter()
 {
-    int lcs[n];
-
-    for(int i=0;i<n;i++) lcs[i]=1;
-
-    int maxLen=1;
-
-    for(int i=1;i<n;i++)
+	int lis[8];
+	for(int i=0;i<n;i++) lis[i]=1;
+	int max_length=0;
+	for(int i=0;i<n;i++)
     {
-        for(int j=0;j<i;j++)
-        {
-            if(arr[i]>arr[j]) lcs[i]=max(lcs[i],lcs[j]+1);
-
-        }
-        maxLen=max(maxLen,lcs[i]);
-    }
-    return maxLen;
+		for(int j=0;j<i;j++)
+		{
+			if(ar[j]<ar[i]) lis[i]=max(lis[i],lis[j]+1);
+		}
+		max_length=max(max_length,lis[i]);
+	}
+	cout << "Longest Increasing Subsequence length is " << max_length << endl;
 }
 
+// dp memorize approach
+int memo[8];
+
+int LISmemo(int pos)
+{
+	if(pos==0) return 1;
+	if(memo[pos]) return memo[pos];
+	int tmp=0;
+	for(int i=pos-1;i>=0;i--)
+	{
+		if(ar[i]<ar[pos]) tmp=max(tmp,LISmemo(i)); 
+	}
+	return memo[pos]=tmp+1;
+}
+
+
+int main(){
+    
+    LISiter();
+    cout << "Longest Increasing Subsequence length is ";
+    cout << LISmemo(n-1) << endl;
+    
+    return 0;
+}
